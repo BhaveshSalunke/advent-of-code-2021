cbb083fb980fe311c225ad09ec35736c0630944b
Bhavesh Salunke <Bhavesh.Salunke@gmail.com>
2021-12-11 13:29:20 +0530

    Refactor day 3 part 2.

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -38,34 +38,22 @@ fun main() {
         return Pair(oneCount, msbList.size - oneCount)
     }
 
-    fun filterChar(msbList: List<String>, i: Int, bit: Char) = msbList.filter { it[i] == bit }
-
-    fun commonBit(input: List<String>, common: Int): List<String> {
-        var msbList = input
-        for (i in 0 until input.first().length) {
-            val (oneCount, zeroCount) = countZeroAndOne(msbList, i)
-            msbList = if (common == 0) {
-                if (zeroCount <= oneCount) {
-                    filterChar(msbList, i, '0')
-                } else {
-                    filterChar(msbList, i, '1')
-                }
-            } else if (zeroCount > oneCount) {
-                filterChar(msbList, i, '0')
-            } else {
-                filterChar(msbList, i, '1')
-            }
-            if (msbList.size == 1) break
+    fun significantBit(significantBit: Int, input: List<String>): List<String> {
+        var list = input
+        for (i in 0 until 4) {
+            if (list.size == 1) break
+            val (oneCount, zeroCount) = countZeroAndOne(list, i)
+            list = if ((significantBit == 0 && zeroCount <= oneCount) || (significantBit == 1 && zeroCount > oneCount))
+                list.filter { it[i] == '0' } else list.filter { it[i] == '1' }
         }
-        return msbList
+        return list
     }
 
     fun part2(input: List<String>): Int {
-        var msbList = commonBit(input, 1)
-        val oxygenGeneratorRating = Integer.parseInt(msbList.first(), 2)
 
-        msbList = commonBit(input, 0)
-        val co2ScrubberRating = Integer.parseInt((msbList.first()), 2)
+        val oxygenGeneratorRating = Integer.parseInt(significantBit(1, input).first(), 2)
+        val co2ScrubberRating = Integer.parseInt((significantBit(0, input).first()), 2)
+
         return oxygenGeneratorRating * co2ScrubberRating
     }
     println(part2(input2))
